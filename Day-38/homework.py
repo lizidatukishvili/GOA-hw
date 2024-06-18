3382b563d21531b0da512ab8fca0a00e55417266
lizidatukishvili <lizi2010.datukishvili@gmail.com>
2024-06-18 12:37:27 +0400

    commit

---
+++ Day-38/homework.py
@@ -0,0 +1,64 @@
+ #დავალება 1
+
+favorite_movies = [
+    "Star wars episode VI",
+    "The Dark Knight",
+    "silicon valley",
+    "star wars epsisode III",
+    "Interstellar",
+    "The Godfather",
+    "Fight Club",
+    "The Matrix",
+    "Forrest Gump",
+    "The Lord of the Rings: The Fellowship of the Ring"
+]
+
+for movie in favorite_movies:
+    print(movie)
+
+#დავალება 2
+
+
+family_members = [
+    "Natia", 
+    "giorgi", 
+    "Lizi", 
+    "sesili",
+    "tazo" 
+    "tato"
+    "nita"
+    "marina"
+    "alberti"
+]
+
+sentence = "My name is {}. My mother's name is {}. My father's name is {}. My sister's name is {}.  My aunt's name is {}. My uncle's name is {}. My cousin's name is {}. My grandmother's name is {}. My grandfather's name is {}.".format(
+    family_members[0], 
+    family_members[1], 
+    family_members[2], 
+    family_members[3], 
+)
+
+print(sentence)
+
+#დავალება 3
+
+friend_names = [
+    "barbare",
+    "tina",
+    "mari",
+    "taso",
+]
+
+
+for friend in friend_names:
+    print(friend)
+
+
+#დავალება 4
+
+group_members = [
+    ("barbare" "tinikashvili"),
+    ("gvanca", "cecxladze"),
+    ("marta", "yipshidze"),
+    ("luka", "natroshvili")
+]
---
+++ Day-39/homework.py
@@ -0,0 +1,11 @@
+numbers = []
+
+
+for i in range(10): 
+ number = int(input("input thy number"))  
+ numbers.append(number)      
+
+
+
+greater_than_100= [] 
+less_than_100 = []
